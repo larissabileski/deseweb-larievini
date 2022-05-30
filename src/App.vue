f24eb6b22813a199369c84424596e032803af865
aluno <vagnercardozo2002@gmail.com>
2022-05-30 15:16:09 -0300

    começado

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>Home | Jogadores | Times</header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  font-weight: normal;
+  height: 100vh;
+}
+
+header,
+footer {
+  background-color: rgb(50, 50, 127);
+  color: rgb(255, 255, 255);
+  height: 15%;
+  font-size: 1.3em;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem
+}
+
+main {
+  height: 70%;
+}
+</style>
